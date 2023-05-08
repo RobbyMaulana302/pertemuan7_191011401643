e71792a8b3e454651f5a6e0658146aa91b97eddc
Ruhyat Ma'rullah <ruhyatmarullah@gmail.com>
2023-05-08 14:36:49 +0700

    pertemuan 7

---
+++ android/app/src/main/kotlin/com/example/pertemuan7_191011401643/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.pertemuan7_191011401643
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyWidget(),
+    );
+  }
+}
+
+class MyWidget extends StatefulWidget {
+  const MyWidget({super.key});
+
+  @override
+  State<MyWidget> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<MyWidget> {
+  List<String> name = [
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine",
+    "ten",
+    "eleven",
+    "twelve"
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("GridView"),
+      ),
+      body: GridView.count(
+        crossAxisCount: 3,
+        children: [
+          for (int i = 0; i < name.length; i++)
+            Container(
+              margin: EdgeInsets.all(20),
+              padding: EdgeInsets.all(10),
+              height: 200,
+              width: 200,
+              alignment: Alignment.topLeft,
+              color: Color.fromARGB(255, i * 15, 100, 100),
+              child: Text(
+                name[i],
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.black,
+                ),
+              ),
+            ),
+        ],
+      ),
+    );
+  }
+}
